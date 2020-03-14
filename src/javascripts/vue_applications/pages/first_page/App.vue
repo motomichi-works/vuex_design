8df98af87cbdd00992cf3920d2e25cfe94189d98
motomichi <motomichi.motomichi@gmail.com>
2020-03-14 15:35:51 +0900

    step1まで完了

---
+++ src/javascripts/vue_applications/pages/first_page/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <TextFieldUnit/>
+  </div>
+</template>
+
+<script>
+import TextFieldUnit from '@/javascripts/vue_applications/common/components/TextFieldUnit'
+export default {
+  name: 'app',
+  components: {
+    TextFieldUnit,
+  },
+}
+</script>
---
+++ src/javascripts/vue_applications/pages/first_page/store.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import textFieldUnit from '@/javascripts/vue_applications/common/modules/textFieldUnit'
+
+Vue.use(Vuex)
+
+const state = () => {
+
+}
+
+const getters = {
+
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions,
+  modules: {
+    textFieldUnit,
+  },
+})
