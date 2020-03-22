92b491de6c96d98dfcdf14c4c39c88b47acf713f
motomichi <motomichi.motomichi@gmail.com>
2020-03-22 23:21:12 +0900

    restrictSubmit.jsを追加

---
+++ src/javascripts/utilities/restrictSubmit.js
@@ -0,0 +1,11 @@
+export default function restrictSubmit (event) {
+  const isEnterKey = event.keyCode === 13;
+  const tagList = [
+    'INPUT',
+  ];
+  const isWillPrevent = tagList.includes(event.target.tagName) && isEnterKey;
+
+  if (isWillPrevent) {
+    event.preventDefault();
+  }
+}
