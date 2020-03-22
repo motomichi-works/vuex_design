7cdc60971c45b1f5150654ec24182793b182e88d
motomichi <motomichi.motomichi@gmail.com>
2020-03-22 23:08:22 +0900

    getLocalState.jsを追加

---
+++ src/javascripts/utilities/getLocalState.js
@@ -0,0 +1,11 @@
+export default function getLocalState (rootState, moduleNames) {
+  const stateObjects = [rootState];
+
+  for (let i = 0; i < moduleNames.length; i++) {
+    stateObjects[i + 1] = stateObjects[i][moduleNames[i]];
+  }
+
+  const localState = stateObjects.pop();
+
+  return localState;
+}
